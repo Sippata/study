076aec5755eb50738749fc6d42e7962b913f817e
Grigory Kaniev <netsafe.g@gmail.com>
2020-10-18 11:02:39 +0500

    Solve tasks from the fp folder

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,11 +5,18 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(_array)
-        0
+        ratings =
+            _array.filter { |film| film["country"].split(",").length >= 2 unless film["country"].nil? }
+                .map { |film| film["rating_kinopoisk"].to_f }
+                .filter { |rating| rating > 0 }
+        rating_sum = ratings.reduce(:+)
+        rating_sum / ratings.length
       end
 
       def chars_count(_films, _threshold)
-        0
+        desired_char = 'и'
+        _films.filter_map { |film| film["name"] if film["rating_kinopoisk"].to_f >= _threshold }
+            .reduce(0) { |acc, name| acc + name.count(desired_char) }
       end
     end
   end
