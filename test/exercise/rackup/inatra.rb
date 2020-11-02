d4f8a7d9a44101d4efaaf8f67376d28f1b88baea
Grigory Kaniev <netsafe.g@gmail.com>
2020-11-02 13:09:42 +0500

    Add solution in rackup folder

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -1,7 +1,27 @@
 module Inatra
   class << self
-    def routes(&block); end
+    def routes(&block)
+      instance_eval(&block)
+    end
 
-    def call(env); end
+    def call(env)
+      method_name = env["REQUEST_METHOD"].downcase + env["PATH_INFO"]
+      if @@routes[method_name].nil?
+        not_found
+      else
+        @@routes[method_name].call
+      end
+    end
+
+    private
+
+    @@routes = Hash.new
+
+    def method_missing(symbol, *args, &block)
+      method_name = symbol.to_s + args[0]
+      @@routes[method_name] = block
+    end
+
+    def not_found; [404, {}, ["Not Found"]] end
   end
 end
